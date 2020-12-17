e9377124f67fa1f85f126667752aaa0923b98818
Scott Blomquist <scott@blomqui.st>
2020-12-16 23:37:15 -0800

    More diagnostics

--- RegExtract/ExtractionPlan.cs
+++ RegExtract/ExtractionPlan.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 
 using RegExtract.ExtractionPlanNodeTypes;
+using RegExtract.RegexTools;
 
 namespace RegExtract
 {
--- RegExtract/RegexCaptureGroupTree.cs
+++ RegExtract/RegexCaptureGroupTree.cs
@@ -5,9 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace RegExtract
+namespace RegExtract.RegexTools
 {
-    internal record RegexCaptureGroupNode(string name, RegexCaptureGroupNode[] children, string rxText)
+    internal record RegexCaptureGroupNode(string name, RegexCaptureGroupNode[] children, ((int start, int length) range, string text) substring)
     {
         public IEnumerable<RegexCaptureGroupNode> NamedGroups => children.Where(node => !int.TryParse(node.name, out var _));
         public IEnumerable<RegexCaptureGroupNode> NumberedGroups => children.Where(node => int.TryParse(node.name, out var _));
@@ -144,7 +144,7 @@ namespace RegExtract
                         else
                         {
                             if (myname == "0") throw new Exception("Too many close parens.");
-                            return new RegexCaptureGroupNode(myname, children.ToArray(), _regexString.Substring(start, loc - start + 1));
+                            return new RegexCaptureGroupNode(myname, children.ToArray(), ((start, loc - start + 1),_regexString.Substring(start, loc - start + 1)));
                         }
                     case '[':
                         loc++;
@@ -165,7 +165,138 @@ namespace RegExtract
             Groups = Groups.OrderBy(name => int.TryParse(name, out var _) ? 0 : 1).ToList();
             Debug.Assert(Groups.Zip(Regex.GetGroupNames(), (a,b) => a == b).All(b => b), "Group List doesn't match Regex.GetGroupNames()");
 
-            return new RegexCaptureGroupNode(myname, children.ToArray(), _regexString.Substring(start, loc - start - 1));
+            return new RegexCaptureGroupNode(myname, children.ToArray(), ((start, loc - start), _regexString.Substring(start, loc - start)));
+        }
+
+        string IntToSubscripts(int i)
+        {
+            List<char> digits = new();
+
+            do
+            {
+                digits.Add((i % 10) switch
+                {
+                    0 => '₀',
+                    1 => '₁',
+                    2 => '₂',
+                    3 => '₃',
+                    4 => '₄',
+                    5 => '₅',
+                    6 => '₆',
+                    7 => '₇',
+                    8 => '₈',
+                    9 => '₉',
+                    _ => throw new InvalidOperationException("That's impossible!")
+                });
+                i /= 10;
+            } while (i > 0);
+
+            digits.Reverse();
+            return string.Join("", digits);
+        }
+
+        public string TreeViz() => String.Join("\n",TreeViz(Tree));
+
+        string[] TreeViz(RegexCaptureGroupNode tree)
+        {
+            var line = tree.substring.text;
+
+            var tag = int.TryParse(tree.name, out var num) ? IntToSubscripts(num) : "";
+
+            char[] pad;
+            string[] results;
+
+            if (!tree.children.Any())
+            {
+                var solo = $"{line}{tag}";
+                results = new[] { solo };
+
+                if (Tree != tree)
+                {
+                    pad = new char[solo.Length];
+                    for (int j = 0; j < pad.Length; j++) pad[j] = '-';
+                    pad[0] = pad[pad.Length - 1] = '↓';
+                    results = new[] { string.Join("",pad), solo  };
+                }
+
+                return results;
+            }
+            else
+            {
+                var left = tree.substring.range.start;
+                var right = tree.substring.range.start + tree.substring.range.length;
+
+                var blocks = tree.children.Select(child => TreeViz(child)).ToArray();
+                var longestblock = blocks.Max(block => block.Length);
+
+                for (int i = 0; i < blocks.Length; i++)
+                {
+                    if (blocks[i].Length != longestblock)
+                    {
+                        var blockline = new char[blocks[i][0].Length];
+                        for (int j = 0; j < blockline.Length; j++) blockline[j] = ' ';
+
+                        var newblock = new string[longestblock];
+                        for (int j = 0; j < newblock.Length; j++)
+                        {
+                            if (j < blocks[i].Length) newblock[j] = blocks[i][j];
+                            else newblock[j] = string.Join("", blockline);
+                        }
+
+                        blocks[i] = newblock;
+                    }
+                }
+
+                var widths = blocks.Select(block => block[0].Length);
+
+                results = new string[blocks[0].Length];
+
+                pad = new char[tree.children[0].substring.range.start - left];
+                for (int j = 0; j < pad.Length; j++) pad[j] = ' ';
+
+                for (int i = 0; i < results.Length; i++)
+                {
+                    results[i] = string.Join("", pad);
+                }
+
+                for (int i = 0; i < blocks.Length; i++)
+                {
+                    var nextstart = i == blocks.Length - 1 ? right : tree.children[i + 1].substring.range.start;
+                    pad = new char[nextstart - (tree.children[i].substring.range.start + tree.children[i].substring.range.length)];
+                    for (int j = 0; j < pad.Length; j++) pad[j] = ' ';
+
+                    for (int j = 0; j < blocks[0].Length; j++)
+                    {
+                        results[j] += blocks[i][j];
+                        results[j] += string.Join("", pad);
+                    }
+                }
+
+                var topline = tree.substring.text.Substring(left, tree.children[0].substring.range.start - left);
+                for (int i = 0; i < blocks.Length; i++)
+                {
+                    pad = new char[blocks[i][0].Length];
+                    for (int j = 0; j < pad.Length; j++) pad[j] = ' ';
+                    topline += string.Join("", pad);
+                    var subleft = tree.children[i].substring.range.start + tree.children[i].substring.range.length;
+                    topline += tree.substring.text.Substring(subleft - left, i == blocks.Length - 1 ? right - subleft : tree.children[i + 1].substring.range.start - subleft);
+                }
+
+                pad = new char[tag.Length];
+                for (int j = 0; j < pad.Length; j++) pad[j] = ' ';
+
+                results = new[] { $"{topline}{tag}" }.Concat(results.Select(result => result + string.Join("", pad))).ToArray();
+
+                if (tree != Tree)
+                {
+                    pad = new char[results[0].Length];
+                    for (int j = 0; j < pad.Length; j++) pad[j] = '-';
+                    pad[0] = pad[pad.Length - 1] = '↓';
+                    results = new[] { string.Join("", pad) }.Concat(results).ToArray();
+                }
+
+                return results;
+            }
         }
     }
 }
